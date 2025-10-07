322e0f34bbf647f7f638da0df5621d7f3170448f
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-10-07 17:45:00 +0200

    deploy script for simplifions

---
+++ deploy_simplifions/deploy.sh
@@ -0,0 +1,16 @@
+cd ~/Apps/datagouv/udata-front-kit/
+
+echo "Push simplifions-preprod branch"
+git co main
+git pull origin main
+git branch -D simplifions-preprod
+git push origin simplifions-preprod -f
+
+echo "Deploy simplifions-prod branch"
+git branch -D simplifions-prod
+git fetch
+git co simplifions-prod
+git merge simplifions-preprod
+git push origin simplifions-prod
+
+echo "Now go to https://github.com/opendatateam/udata-front-kit/actions/workflows/create-deploy-release.yml and trigger new deployments from simplifions-prod and simplifions-preprod"
